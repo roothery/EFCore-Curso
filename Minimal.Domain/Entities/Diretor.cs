6ab65c6e249896be710034616d1331a9aa9458f9
Roothery <roothery@gmail.com>
2025-05-11 18:51:12 -0400

    Seção 7 - EF Core (relacionamentos)

--- Minimal.Domain/Entities/Diretor.cs
+++ Minimal.Domain/Entities/Diretor.cs
@@ -4,5 +4,9 @@ public class Diretor
 {
     public int Id { get; set; }
     public required string Nome { get; set; }
-    public ICollection<Filme> Filmes { get; set; } = [];
+
+    public ICollection<DiretorFilme> DiretoresFilmes { get; set; } = null!;
+    public ICollection<Filme> Filmes { get; set; } = null!;
+
+    public DiretorDetalhe? DiretorDetalhe { get; set; }
 }
--- Minimal.Repo/Migrations/20250511214241_O2O_DiretorDetalhe.Designer.cs
+++ Minimal.Repo/Migrations/20250511214241_O2O_DiretorDetalhe.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Minimal.API.DbContexts;
 
@@ -10,9 +11,11 @@ using Minimal.API.DbContexts;
 namespace Minimal.Repo.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250511214241_O2O_DiretorDetalhe")]
+    partial class O2O_DiretorDetalhe
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
